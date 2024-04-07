fef6fc15d3f56a9059e565da2c19bf31b43d6ec6
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-07 10:11:06 +0300

    script that starts a Flask web application

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -5,8 +5,8 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
 import uuid
+from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -31,17 +31,14 @@ def hbnb():
 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
-
+    cache_id = str(uuid.uuid4())
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-
-    cache_id = uuid.uuid4()
-
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id = cache_id)
 
 
 if __name__ == "__main__":
